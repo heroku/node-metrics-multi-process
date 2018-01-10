dc733da1bbe6e1e290509a259a3011c27f15696a
Jeremy Morrell <jmorrell@heroku.com>
2018-01-10 14:08:30 -0800

    Add throng and split into multi-process

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ require('fuzzy-rotary-phone');
 
 const http = require('http');
 const crypto = require('crypto');
+const throng = require('throng');
 
 const PORT = process.env.PORT || 5000;
+const WORKERS = process.env.WEB_CONCURRENCY || 1;
 
 // This will block the event loop for ~lengths of time
 function blockCpuFor(ms) {
@@ -19,31 +21,39 @@ function blockCpuFor(ms) {
 	}
 }
 
-// block the event loop for 100ms every second
-setInterval(() => {
-  blockCpuFor(100);
-}, 1000)
+throng({
+  workers: WORKERS,
+  lifetime: Infinity
+}, start);
 
-// block the event loop for 1sec every 30 seconds
-setInterval(() => {
-  blockCpuFor(1000);
-}, 30000)
+function start() {
+  // block the event loop for 100ms every second
+  setInterval(() => {
+    blockCpuFor(100);
+  }, 1000)
 
-// Allocate and erase memory on an interval
-let store = [];
+  // block the event loop for 1sec every 30 seconds
+  setInterval(() => {
+    blockCpuFor(1000);
+  }, 30000)
 
-setInterval(() => {
-  store.push(crypto.randomBytes(1000000).toString('hex'));
-}, 500);
+  // Allocate and erase memory on an interval
+  let store = [];
 
-setInterval(() => {
-  store = [];
-}, 60000);
+  setInterval(() => {
+    store.push(crypto.randomBytes(1000000).toString('hex'));
+  }, 500);
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end(`Hello, world!\n`);
-})
+  setInterval(() => {
+    store = [];
+  }, 60000);
 
-server.listen(PORT, () => console.log(`Listening on ${PORT}`));
+  const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(`Hello, world!\n`);
+  })
+
+  server.listen(PORT, () => console.log(`Listening on ${PORT}`));
+}
+
